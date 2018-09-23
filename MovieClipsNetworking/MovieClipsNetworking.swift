5ca656ef159328f99f978adc8535a0338450aaa9
stpdc <43500109+stpdc@users.noreply.github.com>
2018-09-23 16:45:03 -0400

    Initial Commit

---
+++ MovieClipsNetworking/MovieClipsNetworking.swift
@@ -0,0 +1,11 @@
+//
+//  MovieClipsNetworking.swift
+//  MovieClipsNetworking
+//
+//  Created by Chen Hao on 9/23/18.
+//  Copyright © 2018 STPDChen. All rights reserved.
+//
+
+class MovieClipsNetworking {
+
+}
